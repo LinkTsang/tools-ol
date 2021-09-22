cca38f8fb4dcfeac1d0bd401d3cfb4ffacc4a612
Link <linkdoyle@163.com>
2021-09-22 09:42:09 +0800

    [func] Add pages `Home` and `About`.

---
+++ src/pages/About.tsx
@@ -0,0 +1,10 @@
+// Copyright (c) 2021 Bin Tsang
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+function About() {
+    return <div>About</div>;
+}
+
+export default About;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+// Copyright (c) 2021 Bin Tsang
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+function Home() {
+    return <div>Home</div>;
+}
+
+export default Home;
