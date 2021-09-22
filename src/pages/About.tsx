ef1e6f9d47dda8ce2059954c552531c076ba907f
Link <linkdoyle@163.com>
2021-09-22 15:36:19 +0800

    [func] Update `About` page.

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,8 +3,26 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
+import { Card, PageHeader } from "antd";
+import { Helmet } from "react-helmet";
+import { useHistory } from "react-router-dom";
+
 function About() {
-    return <div>About</div>;
+    const history = useHistory();
+    return (
+        <Card>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>关于</title>
+            </Helmet>
+            <PageHeader
+                className="site-page-header"
+                onBack={() => history.push("/tools")}
+                title="关于"
+            />
+            <p>作者很懒，啥也没写~</p>
+        </Card>
+    );
 }
 
 export default About;
