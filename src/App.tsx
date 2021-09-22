8bb1980802944b63802c886a09361d4d939f955c
Link <linkdoyle@163.com>
2021-09-22 14:19:07 +0800

    [func] Redict to `/tools`.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import "antd/dist/antd.css";
 import "./App.css";
 
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import {
+    HashRouter as Router,
+    Switch,
+    Route,
+    Redirect,
+} from "react-router-dom";
 import About from "./pages/About";
-import Home from "./pages/Home";
 import ToolList from "./pages/ToolList";
 
 function App() {
@@ -18,7 +22,7 @@ function App() {
                         <About />
                     </Route>
                     <Route path="/">
-                        <Home />
+                        <Redirect to="/tools" />
                     </Route>
                 </Switch>
             </div>
